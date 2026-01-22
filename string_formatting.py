5f1c2b47007b4ed939de8f492472f72c99d9df4f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-22 16:28:43 +0600

    Add string formatting examples to string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,27 @@
+txt = f"hello {'world'}"
+print(txt)
+
+#
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+#
+price = 59
+tax = 0.25
+txt = f"The price is {price + (price * tax)} dollars"
+print(txt)
+
+#
+
+price = 59
+txt = f"It is very {'Expensive' if price>50 else 'Cheap'}"
+print(txt)
+
+# 
+
+name = "Meghla"
+txt = f"Hello, {name.upper()}"
+print(txt)
+
+# 
+
