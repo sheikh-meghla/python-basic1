6eb0d5af83c4388408587d42866773101fdaf7ff
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-22 18:05:37 +0600

    Add polymorphism.py with examples of string, tuple, and dictionary length

---
+++ polymorphism.py
@@ -0,0 +1,13 @@
+x = "Hello World!"
+print(len(x))
+
+#
+
+mytuple = ("apple", "banana", "cherry")
+print(len(mytuple))
+
+#
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(len(thisdict))
+
