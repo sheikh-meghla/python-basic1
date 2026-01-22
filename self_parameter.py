e3af3903b510d966cad45bdacc5a0d25123a8c15
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-22 17:06:31 +0600

    Add Person class with greeting and welcome methods

---
+++ self_parameter.py
@@ -0,0 +1,27 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+    print("I am a  designer")
+x = Person("Meghla", 17)
+x.greet()
+
+
+#
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    return "Hello, " + self.name
+
+  def welcome(self):
+    message = self.greet()
+    print(message + "! Welcome to our website.")
+
+x = Person("Meghla")
+x.welcome()
