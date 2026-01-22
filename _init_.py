271d906a80b188d0665ab9cefb80d00c1d24f449
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-22 16:56:18 +0600

    python _init_ mathod

---
+++ _init_.py
@@ -0,0 +1,22 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+x = Person("Meghla", 17)
+
+print(x.name)
+print(x.age)
+
+
+#
+
+class Person:
+  def __init__(self, email, phone):
+    self.email = email
+    self.phone = phone
+
+x = Person("meghla@gmail.com", 1302809067)
+
+print(x.email)
+print(x.phone)
