b09ee29640a051a562243173ce265b8a20fe980f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-22 17:50:22 +0600

    Add Student class inheriting from Person with welcome method

---
+++ inheritance.py
@@ -0,0 +1,32 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("Meghla", "Sheikh")
+x.printname()
+
+
+#
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Meghla", "Sheikh", 2024)
+x.welcome()
